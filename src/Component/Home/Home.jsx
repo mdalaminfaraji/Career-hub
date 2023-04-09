07435faca6a7320209bf050e29f671e6e888b9f3
mdalaminfaraji <alaminice1617@gmail.com>
2023-04-10 01:44:39 +0600

    banner secton done

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from 'react'
+
+
+import Banner from '../Banner/Banner'
 
 const Home = () => {
-    return (
-        <div>
-            <h1>Hello I am Home section</h1>
-        </div>
-    );
-};
+  return (
+    <Banner></Banner>
+  )
+}
 
-export default Home;
+export default Home
